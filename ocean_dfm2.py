14b0ea41cbb79604187d20b5b67bb6d6349fbdd3
Rusty Holleman <rustychris@gmail.com>
2018-06-25 18:01:50 -0700

    Spatially variable tides, 3D, no density

--- ocean_dfm2.py
+++ ocean_dfm2.py
@@ -1,7 +1,13 @@
 """
 Reimplementation of ocean_dfm.py, using more recent DFlowModel code.
 """
+import six
+from stompy.spatial import interpXYZ
+from stompy.model.otps import read_otps
 import stompy.model.delft.dflow_model as dfm
+six.moves.reload_module(interpXYZ)
+six.moves.reload_module(read_otps)
+six.moves.reload_module(dfm)
 
 class SFOceanModel(dfm.DFlowModel):
     dfm_bin_dir="/home/rusty/src/dfm/r53925-opt/bin"
@@ -11,7 +17,9 @@ class SFOceanModel(dfm.DFlowModel):
 
     def bc_factory(self,params):
         if params['name']=='Ocean':
-            return dfm.OTPSStageBC(self,otps_model='wc')
+            # apply the same boundary to all of those edges:
+            # return dfm.OTPSStageBC(self,otps_model='wc',**params)
+            return dfm.MultiBC(self,dfm.OTPSStageBC,otps_model='wc',**params)
         else:
             raise Exception("Unrecognized %s"%str(params))
 
@@ -21,8 +29,6 @@ class SFOceanModel(dfm.DFlowModel):
         mdu['geometry','Layertype']=2 # z layers
         mdu['geometry','StretchType']=2 # exponential
         # surface percentage, ignored, bottom percentage
-        # This gives about 2m surface cells, and O(500m) deep layers.
-        #mdu['geometry','StretchCoef']="0.0003 0.02 0.7"
         # this should move the first interface down to below -1m for max depth 1000
         mdu['geometry','StretchCoef']="0.002 0.02 0.7"
         mdu['numerics','Zwsbtol'] = 0.0 # that's the default anyway...
@@ -31,7 +37,6 @@ class SFOceanModel(dfm.DFlowModel):
         # This helps with reconstructing the z-layer geometry, better than
         # trying to duplicate dflowfm layer code.
         mdu['output','FullGridOutput']    = 1
-
 
 model=SFOceanModel()
 model.set_cache_dir('cache')
@@ -44,9 +49,9 @@ model.run_stop =np.datetime64('2017-08-01')
 
 import ragged_grid as grid_mod
 
+model.config_z_layers()
 model.set_grid(grid_mod.grid)
 model.add_bcs_from_features(grid_mod.bc_features)
-
 
 model.write()
 model.partition()
