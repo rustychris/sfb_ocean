181088c3338ffaf1edf18e0889cb19a37540aa91
Rusty Holleman <rustychris@gmail.com>
2019-03-12 16:37:07 -0700

    Bay-ocean combined grid

--- suntans/bathy.py
+++ suntans/bathy.py
@@ -8,7 +8,8 @@ data_root=os.path.join(base,"../../../data/")
 @memoize.memoize()
 def dem():
     srcs=[os.path.join(data_root,"bathy_interp/master2017/tiles_2m_20171024/merged_2m.tif"),
-          os.path.join(data_root,"bathy_dwr/gtiff/dem_bay_delta*.tif")]
+          os.path.join(data_root,"bathy_dwr/gtiff/dem_bay_delta*.tif"),
+          os.path.join(data_root,"ncei/coastal_relief-farallones_clip-utm-merge.tif")]
     mrf=field.MultiRasterField(srcs)
     return mrf
     
