3c20b494ea166a8c786bcf81ab4496c5d1156c5a
Rusty Holleman <rustychris@gmail.com>
2019-08-07 18:52:57 +0000

    Batch ptm conversion script

--- ptm/all_sources/ptm_setup.py
+++ ptm/all_sources/ptm_setup.py
@@ -11,7 +11,7 @@ import numpy as np
 # this run doesn't yet exist on cws-linuxmodeling
 # model=sun_driver.SuntansModel.load("/opt2/sfb_ocean/suntans/runs/merge_017-201706")
 # as far as ec2 has gotten:
-model=sun_driver.SuntansModel.load("/shared2/src/sfb_ocean/suntans/runs/merged_018_20170809")
+model=sun_driver.SuntansModel.load("/shared2/src/sfb_ocean/suntans/runs/merged_018_20170819")
 # model=sun_driver.SuntansModel.load("/opt2/sfb_ocean/suntans/runs/merge_009-20171201")
 # mounting cws-linuxmodeling hard drive externally
 # model=sun_driver.SuntansModel.load("/media/rusty/80c8a8ec-71d2-4687-aa6b-41c23f557be8/sfb_ocean/suntans/runs/merge_009-20170801")
@@ -135,8 +135,7 @@ cfg.end_time=np.datetime64("2017-08-15")
 # cfg.run_dir='compare_sediment' # 3 behaviors at each source, 100 particles/hour
 
 # 5 is for pretty small runs.
-# try 10 for now on aws
-particles_per_interval=10
+particles_per_interval=5
 
 # cfg.run_dir='all_source_select_w' # 3 behaviors at each source.
 cfg.run_dir='all_source_select_w_const_20170615' # constant # particles / hr
